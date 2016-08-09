cf3fdc3890b5fda6bc8a4c7575f67e813b4a46ec
Eric Westphal <BonanzaEric@gmail.com>
2016-08-08 21:41:51 -0400

    Test the jacobians--why didn't I think of this before?

--- ahrs/ahrs_test.go
+++ ahrs/ahrs_test.go
@@ -1,156 +1,151 @@
 package ahrs
 
 import (
-	"fmt"
+	"log"
 	"math"
+	"math/rand"
 	"testing"
-
-	"github.com/westphae/quaternion"
+	"time"
 )
 
-const (
-	Pi = math.Pi
-	Small = 1e-6
-)
+func CreateRandomState() (s *State) {
+	s = &State{
+		U1: rand.Float64()*100+15,
+		U2: rand.Float64()*10-5,
+		U3: rand.Float64()*10-5,
+		Z1: rand.Float64()*1-0.5,
+		Z2: rand.Float64()*1-0.5,
+		Z3: rand.Float64()*1-0.5,
+		E0: rand.Float64(),
+		E1: rand.Float64()*2-1,
+		E2: rand.Float64()*2-1,
+		E3: rand.Float64()*2-1,
+		H1: rand.Float64()*20-10,
+		H2: rand.Float64()*20-10,
+		H3: rand.Float64()*20-10,
+		N1: rand.Float64()*20-10,
+		N2: rand.Float64()*20-10,
+		N3: rand.Float64()*20-10,
 
-func TestRoundTrips(t *testing.T) {
-	phis := []float64{0, 0.1, 0.2, 0.5, 1, 1.5, 2, 2.5, 3, -3, -2, -1, -0.5, -0.2}
-	thetas := []float64{0.1, 0.2, 0.5, 1, 1.5, -1.5, -0.5, -0.2, 0.2, 0.1, -1, -0.5, -0.2, 0}
-	psis := []float64{1, 1.5, 2, 2.5, 3, 4, 0.1, 0.2, 0.5, 5, 5.5, 3.5, 6, 0}
-	var q0, q1, q2, q3 float64
-	var phi, theta, psi float64
-	var phiOut, thetaOut, psiOut float64
+		V1: rand.Float64()*20-10,
+		V2: rand.Float64()*20-10,
+		V3: rand.Float64()*10-5,
+		C1: rand.Float64()*0.1-0.05,
+		C2: rand.Float64()*0.1-0.05,
+		C3: rand.Float64()*0.1-0.05,
+		F0: rand.Float64(),
+		F1: rand.Float64()*2-1,
+		F2: rand.Float64()*2-1,
+		F3: rand.Float64()*2-1,
+		D1: rand.Float64()*0.1-0.05,
+		D2: rand.Float64()*0.1-0.05,
+		D3: rand.Float64()*0.1-0.05,
+		L1: rand.Float64()*1-0.5,
+		L2: rand.Float64()*1-0.5,
+		L3: rand.Float64()*1-0.5,
 
-	for i := 0; i < len(phis); i++ {
-		phi = phis[i]
-		theta = thetas[i]
-		psi = psis[i]
-		q0, q1, q2, q3 = ToQuaternion(phi, theta, psi)
-		phiOut, thetaOut, psiOut = FromQuaternion(q0, q1, q2, q3)
-		if math.Abs(phi-phiOut) > Small || math.Abs(theta-thetaOut) > Small || math.Abs(psi-psiOut) > Small {
-			fmt.Printf("%+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f\n",
-				phi, phiOut, theta, thetaOut, psi, psiOut)
-			t.Fail()
-		}
+		T : 10,
+	}
+	s.normalize()
 
+	return
+}
+
+func stateMap(s *State) (map[int]*float64) {
+	return map[int]*float64{
+		 0: &s.U1,
+		 1: &s.U2,
+		 2: &s.U3,
+		 3: &s.Z1,
+		 4: &s.Z2,
+		 5: &s.Z3,
+		 6: &s.E0,
+		 7: &s.E1,
+		 8: &s.E2,
+		 9: &s.E3,
+		10: &s.H1,
+		11: &s.H2,
+		12: &s.H3,
+		13: &s.N1,
+		14: &s.N2,
+		15: &s.N3,
+		16: &s.V1,
+		17: &s.V2,
+		18: &s.V3,
+		19: &s.C1,
+		20: &s.C2,
+		21: &s.C3,
+		22: &s.F0,
+		23: &s.F1,
+		24: &s.F2,
+		25: &s.F3,
+		26: &s.D1,
+		27: &s.D2,
+		28: &s.D3,
+		29: &s.L1,
+		30: &s.L2,
+		31: &s.L3,
 	}
 }
 
-func TestSpecificToQuaternion(t *testing.T) {
-	var (
-		c30 = math.Sqrt(3) / 2
-		c60 = 0.5
-	)
-	phis :=   []float64{ 0,    0,  0,      0,      0,    0,        0,  Pi/3, Pi/3, -2*Pi/3}
-	thetas := []float64{ 0,    0,  0,      0,      0, Pi/3,    -Pi/3,     0,    0,       0}
-	psis :=   []float64{ 0, Pi/2, Pi, 4*Pi/3, 3*Pi/2, Pi/2,   5*Pi/3,  Pi/2,    0,      Pi}
-	u1s :=    []float64{ 0,    1,  0,   -c30,     -1,  c60, -c30*c60,     1,    0,       0}
-	u2s :=    []float64{ 1,    0, -1,   -c60,      0,    0,  c60*c60,     0,    1,      -1}
-	u3s :=    []float64{ 0,    0,  0,      0,      0,  c30,     -c30,     0,    0,       0}
-	v1s :=    []float64{-1,    0,  1,    c60,      0,    0,     -c60,     0, -c60,    -c60}
-	v2s :=    []float64{ 0,    1,  0,   -c30,     -1,    1,     -c30,   c60,    0,       0}
-	v3s :=    []float64{ 0,    0,  0,      0,      0,    0,        0,   c30,  c30,    -c30}
-	w1s :=    []float64{ 0,    0,  0,      0,      0, -c30, -c30*c30,     0,  c30,     c30}
-	w2s :=    []float64{ 0,    0,  0,      0,      0,    0,  c60*c30,  -c30,    0,       0}
-	w3s :=    []float64{ 1,    1,  1,      1,      1,  c60,      c60,   c60,  c60,    -c60}
-
-	x := quaternion.Quaternion{X: 1}
-	y := quaternion.Quaternion{Y: 1}
-	z := quaternion.Quaternion{Z: 1}
-	var (
-		e0, e1, e2, e3         float64
-		u, v, w, e, uu, vv, ww quaternion.Quaternion
-	)
-
-	for i := 0; i < len(phis); i++ {
-		u = quaternion.Quaternion{X: u1s[i], Y: u2s[i], Z: u3s[i]}
-		v = quaternion.Quaternion{X: v1s[i], Y: v2s[i], Z: v3s[i]}
-		w = quaternion.Quaternion{X: w1s[i], Y: w2s[i], Z: w3s[i]}
-		e0, e1, e2, e3 = ToQuaternion(phis[i], thetas[i], psis[i])
-		e = quaternion.Quaternion{W: e0, X: e1, Y: e2, Z: e3}
-		uu = quaternion.Prod(e, x, quaternion.Conj(e))
-		vv = quaternion.Prod(e, y, quaternion.Conj(e))
-		ww = quaternion.Prod(e, z, quaternion.Conj(e))
-
-		if math.Abs(u.W-uu.W) > Small || math.Abs(u.X-uu.X) > Small ||
-			math.Abs(u.Y-uu.Y) > Small || math.Abs(u.Z-uu.Z) > Small {
-			fmt.Printf("%d u: %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f\n",
-				i, u.W, uu.W, u.X, uu.X, u.Y, uu.Y, u.Z, uu.Z)
-			t.Fail()
-		}
-
-		if math.Abs(v.W-vv.W) > Small || math.Abs(v.X-vv.X) > Small ||
-			math.Abs(v.Y-vv.Y) > Small || math.Abs(v.Z-vv.Z) > Small {
-			fmt.Printf("%d v: %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f\n",
-				i, v.W, vv.W, v.X, vv.X, v.Y, vv.Y, v.Z, vv.Z)
-			t.Fail()
-		}
-
-		if math.Abs(w.W-ww.W) > Small || math.Abs(w.X-ww.X) > Small ||
-			math.Abs(w.Y-ww.Y) > Small || math.Abs(w.Z-ww.Z) > Small {
-			fmt.Printf("%d w: %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f\n",
-				i, w.W, ww.W, w.X, ww.X, w.Y, ww.Y, w.Z, ww.Z)
-			t.Fail()
-		}
+func measMap(m *Measurement) (map[int]*float64) {
+	return map[int]*float64{
+		 0: &m.U1,
+		 1: &m.U2,
+		 2: &m.U3,
+		 3: &m.W1,
+		 4: &m.W2,
+		 5: &m.W3,
+		 6: &m.A1,
+		 7: &m.A2,
+		 8: &m.A3,
+		 9: &m.B1,
+		10: &m.B2,
+		11: &m.B3,
+		12: &m.M1,
+		13: &m.M2,
+		14: &m.M3,
 	}
 }
 
-// A negative pitch rate about the y-axis should pitch the nose up
-func TestPitchRotationQuaternion(t *testing.T) {
+func TestJacobianMeasurement(t *testing.T) {
+	// U*3, Z*3, E*4, H*3, N*3,
+	// V*3, C*3, F*4, D*3, L*3
+	// U*3, W*3, A*3, B*3, M*3
 
-	q_nose_aircraft := quaternion.Quaternion{0, 1, 0, 0}
-	q_rt_wing_aircraft := quaternion.Quaternion{0, 0, -1, 0}
-	q_ae := quaternion.Quaternion{1, 0, 0, 0} // headed north
-	h_a := quaternion.Quaternion{1, 0, -0.5* Pi /180, 0}
+	rand.Seed(time.Now().Unix())
+	s := CreateRandomState()
+	s.F0 = 1
+	s.F1 = 0
+	s.F2 = 0
+	s.F3 = 0
+	s.normalize()
 
-	q_nose_pitched_a := quaternion.Prod(h_a, q_nose_aircraft, quaternion.Conj(h_a))
-	q_nose_pitched_e := quaternion.Prod(quaternion.Conj(q_ae), q_nose_pitched_a, q_ae)
-	q_rt_wing_pitched_a := quaternion.Prod(h_a, q_rt_wing_aircraft, quaternion.Conj(h_a))
-	q_rt_wing_pitched_e := quaternion.Prod(quaternion.Conj(q_ae), q_rt_wing_pitched_a, q_ae)
-	if q_nose_pitched_e.Z < Small || math.Abs(q_nose_pitched_e.Y) > Small ||
-			math.Abs(q_rt_wing_pitched_e.X) > Small || math.Abs(q_rt_wing_pitched_e.Z) > Small {
-		fmt.Println(q_nose_pitched_e)
-		fmt.Println(q_rt_wing_pitched_e)
-		t.Fail()
-	}
-}
+	m := s.PredictMeasurement()
+	h := s.CalcJacobianMeasurement()
 
-// A positive roll rate about the x-axis should roll the right wing down
-func TestRollRotationQuaternion(t *testing.T) {
+	smap := stateMap(s)
+	mmap := measMap(m)
 
-	q_nose_aircraft := quaternion.Quaternion{0, 1, 0, 0}
-	q_rt_wing_aircraft := quaternion.Quaternion{0, 0, -1, 0}
-	q_ae := quaternion.Quaternion{1, 0, 0, 0} // headed north
-	h_a := quaternion.Quaternion{1, 0.5* Pi /180, 0, 0}
-
-	q_nose_rolled_a := quaternion.Prod(h_a, q_nose_aircraft, quaternion.Conj(h_a))
-	q_nose_rolled_e := quaternion.Prod(quaternion.Conj(q_ae), q_nose_rolled_a, q_ae)
-	q_rt_wing_rolled_a := quaternion.Prod(h_a, q_rt_wing_aircraft, quaternion.Conj(h_a))
-	q_rt_wing_rolled_e := quaternion.Prod(quaternion.Conj(q_ae), q_rt_wing_rolled_a, q_ae)
-	if math.Abs(q_nose_rolled_e.Z) > Small || math.Abs(q_nose_rolled_e.Y) > Small ||
-			q_rt_wing_rolled_e.Z > Small || math.Abs(q_rt_wing_rolled_e.X) > Small {
-		fmt.Println(q_nose_rolled_e)
-		fmt.Println(q_rt_wing_rolled_e)
-		t.Fail()
-	}
-}
-
-// A negative yaw rate about the z-axis should turn the nose to the right
-func TestYawRotationQuaternion(t *testing.T) {
-
-	q_nose_aircraft := quaternion.Quaternion{0, 1, 0, 0}
-	q_rt_wing_aircraft := quaternion.Quaternion{0, 0, -1, 0}
-	q_ae := quaternion.Quaternion{1, 0, 0, 0} // headed north
-	h_a := quaternion.Quaternion{1, 0, 0, -0.5* Pi /180}
-
-	q_nose_yawed_a := quaternion.Prod(h_a, q_nose_aircraft, quaternion.Conj(h_a))
-	q_nose_yawed_e := quaternion.Prod(quaternion.Conj(q_ae), q_nose_yawed_a, q_ae)
-	q_rt_wing_yawed_a := quaternion.Prod(h_a, q_rt_wing_aircraft, quaternion.Conj(h_a))
-	q_rt_wing_yawed_e := quaternion.Prod(quaternion.Conj(q_ae), q_rt_wing_yawed_a, q_ae)
-	if math.Abs(q_nose_yawed_e.Z) > Small || q_nose_yawed_e.X < Small ||
-			math.Abs(q_rt_wing_yawed_e.Z) > Small || q_rt_wing_yawed_e.X > -Small {
-		fmt.Println(q_nose_yawed_e)
-		fmt.Println(q_rt_wing_yawed_e)
-		t.Fail()
+	for i:=0; i<32; i++ {
+		if (i >= 12 && i <= 14) || (i >= 29) { //TODO westphae: don't skip these after working out Jacobian
+			continue
+		}
+		*(smap[i]) += Small
+		s.calcRotationMatrices()
+		mm := s.PredictMeasurement()
+		*(smap[i]) -= Small
+		s.calcRotationMatrices()
+		mmmap := measMap(mm)
+		for j:=0; j<12; j++ { //TODO westphae: indices all the way up to 15
+			dM := (*(mmmap[j]) - *(mmap[j])) / Small
+			if math.Abs(dM - h.Get(j, i)) > 1e-4 {
+				log.Println(s.f11, s.f12, s.f13)
+				log.Println(s.f21, s.f22, s.f23)
+				log.Println(s.f31, s.f32, s.f33)
+				log.Printf("Error in index %d,%d: Calc %6f, Jacobian was %6f\n", j, i, dM, h.Get(j, i))
+				t.Fail()
+			}
+		}
 	}
 }
---
+++ ahrs/quaternions_test.go
@@ -0,0 +1,151 @@
+package ahrs
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/westphae/quaternion"
+)
+
+func TestRoundTrips(t *testing.T) {
+	phis := []float64{0, 0.1, 0.2, 0.5, 1, 1.5, 2, 2.5, 3, -3, -2, -1, -0.5, -0.2}
+	thetas := []float64{0.1, 0.2, 0.5, 1, 1.5, -1.5, -0.5, -0.2, 0.2, 0.1, -1, -0.5, -0.2, 0}
+	psis := []float64{1, 1.5, 2, 2.5, 3, 4, 0.1, 0.2, 0.5, 5, 5.5, 3.5, 6, 0}
+	var q0, q1, q2, q3 float64
+	var phi, theta, psi float64
+	var phiOut, thetaOut, psiOut float64
+
+	for i := 0; i < len(phis); i++ {
+		phi = phis[i]
+		theta = thetas[i]
+		psi = psis[i]
+		q0, q1, q2, q3 = ToQuaternion(phi, theta, psi)
+		phiOut, thetaOut, psiOut = FromQuaternion(q0, q1, q2, q3)
+		if math.Abs(phi-phiOut) > Small || math.Abs(theta-thetaOut) > Small || math.Abs(psi-psiOut) > Small {
+			fmt.Printf("%+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f\n",
+				phi, phiOut, theta, thetaOut, psi, psiOut)
+			t.Fail()
+		}
+
+	}
+}
+
+func TestSpecificToQuaternion(t *testing.T) {
+	var (
+		c30 = math.Sqrt(3) / 2
+		c60 = 0.5
+	)
+	phis :=   []float64{ 0,    0,  0,      0,      0,    0,        0,  Pi/3, Pi/3, -2*Pi/3}
+	thetas := []float64{ 0,    0,  0,      0,      0, Pi/3,    -Pi/3,     0,    0,       0}
+	psis :=   []float64{ 0, Pi/2, Pi, 4*Pi/3, 3*Pi/2, Pi/2,   5*Pi/3,  Pi/2,    0,      Pi}
+	u1s :=    []float64{ 0,    1,  0,   -c30,     -1,  c60, -c30*c60,     1,    0,       0}
+	u2s :=    []float64{ 1,    0, -1,   -c60,      0,    0,  c60*c60,     0,    1,      -1}
+	u3s :=    []float64{ 0,    0,  0,      0,      0,  c30,     -c30,     0,    0,       0}
+	v1s :=    []float64{-1,    0,  1,    c60,      0,    0,     -c60,     0, -c60,    -c60}
+	v2s :=    []float64{ 0,    1,  0,   -c30,     -1,    1,     -c30,   c60,    0,       0}
+	v3s :=    []float64{ 0,    0,  0,      0,      0,    0,        0,   c30,  c30,    -c30}
+	w1s :=    []float64{ 0,    0,  0,      0,      0, -c30, -c30*c30,     0,  c30,     c30}
+	w2s :=    []float64{ 0,    0,  0,      0,      0,    0,  c60*c30,  -c30,    0,       0}
+	w3s :=    []float64{ 1,    1,  1,      1,      1,  c60,      c60,   c60,  c60,    -c60}
+
+	x := quaternion.Quaternion{X: 1}
+	y := quaternion.Quaternion{Y: 1}
+	z := quaternion.Quaternion{Z: 1}
+	var (
+		e0, e1, e2, e3         float64
+		u, v, w, e, uu, vv, ww quaternion.Quaternion
+	)
+
+	for i := 0; i < len(phis); i++ {
+		u = quaternion.Quaternion{X: u1s[i], Y: u2s[i], Z: u3s[i]}
+		v = quaternion.Quaternion{X: v1s[i], Y: v2s[i], Z: v3s[i]}
+		w = quaternion.Quaternion{X: w1s[i], Y: w2s[i], Z: w3s[i]}
+		e0, e1, e2, e3 = ToQuaternion(phis[i], thetas[i], psis[i])
+		e = quaternion.Quaternion{W: e0, X: e1, Y: e2, Z: e3}
+		uu = quaternion.Prod(e, x, quaternion.Conj(e))
+		vv = quaternion.Prod(e, y, quaternion.Conj(e))
+		ww = quaternion.Prod(e, z, quaternion.Conj(e))
+
+		if math.Abs(u.W-uu.W) > Small || math.Abs(u.X-uu.X) > Small ||
+			math.Abs(u.Y-uu.Y) > Small || math.Abs(u.Z-uu.Z) > Small {
+			fmt.Printf("%d u: %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f\n",
+				i, u.W, uu.W, u.X, uu.X, u.Y, uu.Y, u.Z, uu.Z)
+			t.Fail()
+		}
+
+		if math.Abs(v.W-vv.W) > Small || math.Abs(v.X-vv.X) > Small ||
+			math.Abs(v.Y-vv.Y) > Small || math.Abs(v.Z-vv.Z) > Small {
+			fmt.Printf("%d v: %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f\n",
+				i, v.W, vv.W, v.X, vv.X, v.Y, vv.Y, v.Z, vv.Z)
+			t.Fail()
+		}
+
+		if math.Abs(w.W-ww.W) > Small || math.Abs(w.X-ww.X) > Small ||
+			math.Abs(w.Y-ww.Y) > Small || math.Abs(w.Z-ww.Z) > Small {
+			fmt.Printf("%d w: %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f, %+5.3f -> %+5.3f\n",
+				i, w.W, ww.W, w.X, ww.X, w.Y, ww.Y, w.Z, ww.Z)
+			t.Fail()
+		}
+	}
+}
+
+// A negative pitch rate about the y-axis should pitch the nose up
+func TestPitchRotationQuaternion(t *testing.T) {
+
+	q_nose_aircraft := quaternion.Quaternion{0, 1, 0, 0}
+	q_rt_wing_aircraft := quaternion.Quaternion{0, 0, -1, 0}
+	q_ae := quaternion.Quaternion{1, 0, 0, 0} // headed north
+	h_a := quaternion.Quaternion{1, 0, -0.5* Pi /180, 0}
+
+	q_nose_pitched_a := quaternion.Prod(h_a, q_nose_aircraft, quaternion.Conj(h_a))
+	q_nose_pitched_e := quaternion.Prod(quaternion.Conj(q_ae), q_nose_pitched_a, q_ae)
+	q_rt_wing_pitched_a := quaternion.Prod(h_a, q_rt_wing_aircraft, quaternion.Conj(h_a))
+	q_rt_wing_pitched_e := quaternion.Prod(quaternion.Conj(q_ae), q_rt_wing_pitched_a, q_ae)
+	if q_nose_pitched_e.Z < Small || math.Abs(q_nose_pitched_e.Y) > Small ||
+			math.Abs(q_rt_wing_pitched_e.X) > Small || math.Abs(q_rt_wing_pitched_e.Z) > Small {
+		fmt.Println(q_nose_pitched_e)
+		fmt.Println(q_rt_wing_pitched_e)
+		t.Fail()
+	}
+}
+
+// A positive roll rate about the x-axis should roll the right wing down
+func TestRollRotationQuaternion(t *testing.T) {
+
+	q_nose_aircraft := quaternion.Quaternion{0, 1, 0, 0}
+	q_rt_wing_aircraft := quaternion.Quaternion{0, 0, -1, 0}
+	q_ae := quaternion.Quaternion{1, 0, 0, 0} // headed north
+	h_a := quaternion.Quaternion{1, 0.5* Pi /180, 0, 0}
+
+	q_nose_rolled_a := quaternion.Prod(h_a, q_nose_aircraft, quaternion.Conj(h_a))
+	q_nose_rolled_e := quaternion.Prod(quaternion.Conj(q_ae), q_nose_rolled_a, q_ae)
+	q_rt_wing_rolled_a := quaternion.Prod(h_a, q_rt_wing_aircraft, quaternion.Conj(h_a))
+	q_rt_wing_rolled_e := quaternion.Prod(quaternion.Conj(q_ae), q_rt_wing_rolled_a, q_ae)
+	if math.Abs(q_nose_rolled_e.Z) > Small || math.Abs(q_nose_rolled_e.Y) > Small ||
+			q_rt_wing_rolled_e.Z > Small || math.Abs(q_rt_wing_rolled_e.X) > Small {
+		fmt.Println(q_nose_rolled_e)
+		fmt.Println(q_rt_wing_rolled_e)
+		t.Fail()
+	}
+}
+
+// A negative yaw rate about the z-axis should turn the nose to the right
+func TestYawRotationQuaternion(t *testing.T) {
+
+	q_nose_aircraft := quaternion.Quaternion{0, 1, 0, 0}
+	q_rt_wing_aircraft := quaternion.Quaternion{0, 0, -1, 0}
+	q_ae := quaternion.Quaternion{1, 0, 0, 0} // headed north
+	h_a := quaternion.Quaternion{1, 0, 0, -0.5* Pi /180}
+
+	q_nose_yawed_a := quaternion.Prod(h_a, q_nose_aircraft, quaternion.Conj(h_a))
+	q_nose_yawed_e := quaternion.Prod(quaternion.Conj(q_ae), q_nose_yawed_a, q_ae)
+	q_rt_wing_yawed_a := quaternion.Prod(h_a, q_rt_wing_aircraft, quaternion.Conj(h_a))
+	q_rt_wing_yawed_e := quaternion.Prod(quaternion.Conj(q_ae), q_rt_wing_yawed_a, q_ae)
+	if math.Abs(q_nose_yawed_e.Z) > Small || q_nose_yawed_e.X < Small ||
+			math.Abs(q_rt_wing_yawed_e.Z) > Small || q_rt_wing_yawed_e.X > -Small {
+		fmt.Println(q_nose_yawed_e)
+		fmt.Println(q_rt_wing_yawed_e)
+		t.Fail()
+	}
+}
