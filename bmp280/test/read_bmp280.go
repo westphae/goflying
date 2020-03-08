02987974ecf2fc5ac804066333b414370dd44a24
Eric Westphal <BonanzaEric@gmail.com>
2020-03-08 15:24:39 -0400

    read_bmp280.go can read from multiple chips

--- bmp280/test/read_bmp280.go
+++ bmp280/test/read_bmp280.go
@@ -12,21 +12,29 @@ func main() {
 	var cur *bmp280.BMPData
 
 	i2cbus := embd.NewI2CBus(1)
-	bmp, err := bmp280.NewBMP280(&i2cbus, bmp280.Address1,
-		bmp280.NormalMode, bmp280.StandbyTime63ms, bmp280.FilterCoeff16, bmp280.Oversamp16x, bmp280.Oversamp16x)
-	if err != nil {
-		fmt.Printf("Error: couldn't initialize BMP280: %s\n", err)
+
+	var bmps []*bmp280.BMP280
+	for i, address:=range([]byte{bmp280.Address1,bmp280.Address2}) {
+		bmp, err := bmp280.NewBMP280(&i2cbus, address,
+			bmp280.NormalMode, bmp280.StandbyTime63ms, bmp280.FilterCoeff16, bmp280.Oversamp16x, bmp280.Oversamp16x)
+		if err != nil {
+			fmt.Printf("no BMP280 at address %d: %s\n", i, err)
+			continue
+		}
+		bmps = append(bmps, bmp)
+		defer bmp.Close()
+	}
+	if len(bmps)==0 {
 		return
 	}
 
-	defer bmp.Close()
-	//fmt.Println("BMP280 initialized successfully")
-	fmt.Println("t,temp,press,alt")
-
-	clock := time.NewTicker(bmp.Delay)
+	fmt.Println("t,chip,temp,press,alt")
+	clock := time.NewTicker(bmps[0].Delay)
 	for {
 		<-clock.C
-		cur = <-bmp.C
-		fmt.Printf("%v,%3.2f,%4.2f,%5.1f\n", cur.T, cur.Temperature, cur.Pressure, bmp280.CalcAltitude(cur.Pressure))
+		for _, bmp:=range(bmps) {
+			cur = <-bmp.C
+			fmt.Printf("%v,%X,%3.2f,%4.2f,%5.1f\n", cur.T, bmp.Address, cur.Temperature, cur.Pressure, bmp280.CalcAltitude(cur.Pressure))
+		}
 	}
 }
